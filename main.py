070af4a7c684c5fa096a950d8b7a42f9bddb93b0
Luiz Guilherme <115933235+lvxzxn@users.noreply.github.com>
2024-09-17 09:28:29 -0300

    :tada: commit inicial

--- main.py
+++ main.py
@@ -6,38 +6,36 @@ from fake_useragent import UserAgent
 
 ua = UserAgent()
 
-def scrape_proxies(file_path: str):
-    apis = [
-        'https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all',
-        'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt',
-        'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt'
-    ]
-    
-    with open(file_path, 'w') as file:
-        for host in apis:
-            try:
-                req = requests.get(host)
-                content = req.text
-                file.write(f'{content.strip()}\n')
-            except requests.RequestException as e:
-                print(f"Failed to fetch proxies from {host}: {e}")
+def scrape_proxies():
+  apis = {
+    'https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all',
+    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt',
+    'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt'
+  }
+  
+  for host in apis:
+    req = requests.get(host)
+    content = req.text
 
 def get_host(hostname: str, proxy: str):
-    if 'http://' in proxy:
-        proxies = {'http': proxy.strip()}
-        headers = {'User-Agent': ua.random}
-        host = proxy.replace('http://', '').strip()
-        ip = host.split(':')[0].strip()
-        port = host.split(':')[1].strip()
-        try:
-            r = requests.get(hostname, headers=headers, proxies=proxies)
-            if r.text:
-                print(f'[{ip}:{port}] | {hostname} - success!')
-            else:
-                return
-        except requests.RequestException as e:
-            print(f'[{ip}:{port}] | {hostname} - failed: {e}')
-            return
+    if len(proxy) < 3:
+      return
+    if not 'http://' in proxy:
+      proxy = f'http://{proxy}'
+      proxies = {'http': proxy.strip()}
+      headers = {'User-Agent': ua.random}
+      host = proxy.replace('http://', '').strip()
+      ip = host.split(':')[0].strip()
+      port = host.split(':')[1].strip()
+      try:
+          r = requests.get(hostname, headers=headers, proxies=proxies)
+          if r.text:
+              print(f'[{ip}:{port}] | {hostname} - success!')
+          else:
+              return
+      except requests.RequestException as e:
+          print(f'[{ip}:{port}] | {hostname} - failed: {e}')
+          return
 
 def load_proxies_and_get(host: str, proxies_file_path: str):
     while True:
@@ -66,21 +64,9 @@ def load_proxies_and_get(host: str, proxies_file_path: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check proxies with a given host.')
-    parser.add_argument('host', type=str, nargs='?', help='The URL of the host to check.')
-    parser.add_argument('proxies_file', type=str, nargs='?', help='Path to the file containing the list of proxies.')
-    parser.add_argument('--update', action='store_true', help='Update the list of proxies from the external sources.')
-
+    parser.add_argument('host', type=str, help='The URL of the host to check.')
+    parser.add_argument('proxies_file', type=str, help='Path to the file containing the list of proxies.')
+    
     args = parser.parse_args()
-
-    if args.update:
-        if args.host or args.proxies_file:
-            print("Cannot use --update with host or proxies_file arguments.")
-        else:
-            scrape_proxies('./http.txt')
-            print(f"Proxies updated and saved to ./http.txt")
-    else:
-        if not args.host or not args.proxies_file:
-            parser.print_help()
-            print("Error: --host and --proxies_file are required if not updating proxies.")
-        else:
-            load_proxies_and_get(args.host, args.proxies_file)
+    
+    load_proxies_and_get(args.host, args.proxies_file)
